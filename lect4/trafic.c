2602c944a0931efa9daed8dbb0b3999e9c4eccd4
Rahul Chakravati <chakravatirahul7@gmail.com>
2025-10-29 20:14:13 +0530

    switch

---
+++ lect4/trafic.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    char r,o,g,signal;
+    printf("Enter colour of traffic light:\nred=r\norange=o\ngreen=g\n");
+    scanf("%c",&signal);
+    switch(signal)
+    {
+        case 'r':printf("STOP");
+        break;
+         case 'o':printf("WAIT");
+        break;
+         case 'g':printf("GOO");
+        break;
+        default:printf("Invalid colour");
+    }
+
+}
+
