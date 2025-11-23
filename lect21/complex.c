a9bbce8855a3509188f7896dc418f3d0b97bb1f9
Rahul Chakravati <chakravatirahul7@gmail.com>
2025-11-23 11:53:42 +0530

    ascii

---
+++ lect21/complex.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[]="rahul";
+    char target='u';
+    char newstr[]
+    for(int i=0; i<ln; i++){
+          if(str[i]!=target){
+            newstr[j]=str[i]
+            j++;
+          }
+    }
+}
